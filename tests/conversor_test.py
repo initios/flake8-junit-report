917a870317ad092dfa9255917eac9f1add5c6c19
Carlos Goce <carlosgoce@gmail.com>
2015-03-17 20:11:02 +0100

    Added first test case

--- tests/conversor_test.py
+++ tests/conversor_test.py
@@ -1,6 +1,8 @@
 import os
 import unittest
 
+from junitConversor import _parse
+
 
 current_dir = os.path.dirname(os.path.realpath(__file__))
 example_files_dir = os.path.join(current_dir, 'flake8_example_results')
@@ -8,5 +10,11 @@ failed_flake8 = os.path.join(example_files_dir, 'failed_flake8.txt')
 
 
 class ConversorTest(unittest.TestCase):
-    def test_converts_a_flake8_file_to_junit(self):
-        assert True
+    def test_can_parse_a_flake8_file(self):
+        parsed = _parse(failed_flake8)
+
+        self.assertEqual(parsed, [
+            {'file': 'tests/subject/__init__.py', 'line': '1', 'col': '1', 'detail': "F401 'os' imported but unused"},
+            {'file': 'tests/subject/__init__.py', 'line': '3', 'col': '1', 'detail': "E302 expected 2 blank lines, found 1"},
+            {'file': 'tests/subject/example.py', 'line': '4', 'col': '1', 'detail': "E302 expected 2 blank lines, found 1"},
+        ])
