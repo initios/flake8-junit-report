a5b9b291532a1d65e5e5b11c695750391daf0523
Carlos Goce <carlosgoce@gmail.com>
2015-03-18 12:28:54 +0100

    Refactor module junitConversor to junit_conversor

---
+++ junit_conversor/__about__.py
@@ -0,0 +1,7 @@
+__title__ = 'flake8-junit-report'
+__summary__ = 'Simple tool that converts a flake8 file to junit format'
+__version__ = '0.0.1'
+__license__ = 'BSD 3-Clause License'
+__uri__ = 'https://github.com/initios/flake8-junit-report'
+__author__ = 'Carlos Goce'
+__email__ = 'carlosgoce@gmail.com'
