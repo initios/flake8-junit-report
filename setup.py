f4d79cc06ba7937600bb76949c1dc4fbe320ce70
Carlos Goce <carlosgoce@gmail.com>
2015-03-27 17:15:59 +0100

    Add Python 2.6 to setup.py

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setuptools.setup(
 
     classifiers=[
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
         'Development Status :: 5 - Production/Stable',
