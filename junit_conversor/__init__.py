549540a628dc1713b4ede34da7792f35331eef0a
Christoph Giesel <cgiesel@bloopark.de>
2015-06-03 23:03:56 +0200

    [ADD] added options --allways-create/-c for creating a file even if input file is empty and --all-files/-a for adding all python files of a directory as test cases

--- junit_conversor/__init__.py
+++ junit_conversor/__init__.py
@@ -1,11 +1,32 @@
 import xml.etree.cElementTree as ET
+import os
 import sys
 from collections import defaultdict
+import click
 
 
-def _parse(file_name):
+def _prepare_file_tree(path):
+    tree = defaultdict(list)
+
+    if not os.path.exists(path):
+        click.echo('Given file path does not exists. Using only files '
+                   'with errors from flake8 output.', err=True)
+        return tree
+
+    for (directory, _, files) in os.walk(path):
+        for f in files:
+            if f.endswith('.py'):
+                tree[os.path.join(directory, f)] = []
+
+    return tree
+
+
+def _parse(file_name, all_files_path=False):
     lines = tuple(open(file_name, 'r'))
     parsed = defaultdict(list)
+
+    if all_files_path:
+        parsed = _prepare_file_tree(all_files_path)
 
     for line in lines:
         splitted = line.split(":")
@@ -25,10 +46,10 @@ def _parse(file_name):
     return dict(parsed)
 
 
-def _convert(origin, destination):
-    parsed = _parse(origin)
+def _convert(origin, destination, allways_create=False, all_files_path=False):
+    parsed = _parse(origin, all_files_path=all_files_path)
 
-    if len(parsed) < 1:
+    if len(parsed) < 1 and not allways_create:
         return
 
     testsuite = ET.Element("testsuite")
